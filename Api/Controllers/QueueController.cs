bdd95766ab470bbeebcb93767a310536384f84af
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-09-27 15:25:12 +0300

    Create queue controller to test whether authentication with JWT works

---
+++ Api/Controllers/QueueController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PSI.Api.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class QueueController : ControllerBase
+   {
+      [Authorize]
+      [HttpPost]
+      public IActionResult JoinQueue()
+      {
+         return Ok("Joined");
+      }
+         
+   }
+}
