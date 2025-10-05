76ce7d92b49723a05eb76848c1aa51cd9a9a1abc
testyt419 <testytgaming@gmail.com>
2025-10-05 14:40:38 +0300

    Create a lobby with an id

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -5,30 +5,6 @@ import Start from "./Start";
 import Home from "./Home";
 import Queue from "./Queue";
 import LobbyPage from "./LobbyPage";
-function Home() {
-  const navigate = useNavigate();
-
-
-  const goToQueue = () => {
-    // FIXME: enter the player into queue logic here
-    navigate("/queue");
-  };
-  const goToLobbyPage = () => {
-    const randomCode = Math.random().toString(36).substring(2, 8).toUpperCase();
-    navigate(`/match/${randomCode}`);
-  };
-
-
-  return (
-    <div>
-      <h1>Main page</h1>
-      <button onClick={goToQueue} className="linkButton">Queue</button>
-      <button onClick={goToLobbyPage} className="linkButton">Create Match Lobby</button>
-
-    </div>
-  );
-}
-
 
 function App() {
   return (
--- ClientApp/src/Home.jsx
+++ ClientApp/src/Home.jsx
@@ -1,47 +1,65 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Home() {
-  const navigate = useNavigate();
-  
-  const handleQueueJoin = async () => {
-   const token = localStorage.getItem("guestToken");
+   const navigate = useNavigate();
+   const [lobbyID, setlobbyID] = useState("");
 
-   if(!token) {
-      alert("You must be logged in to access the queue.");
-      return;
-   }
+   const handleQueueJoin = async () => {
+      const token = localStorage.getItem("guestToken");
 
-   try {
-      const response = await fetch("http://localhost:5243/api/queue", {
-         method: "POST",
-         headers: {
-            "Authorization": "Bearer " + token,
-            "Content-Type": "application/json"
-         },
-      });
-
-      if(!response.ok) {
-         console.log("Status: ", response.status);
-         const errorText = await response.text();
-         console.log("Error text: ", errorText)
-         alert("Failed to join queue: " + (errorText || "Status " + response.status));
+      if (!token) {
+         alert("You must be logged in to access the queue.");
          return;
       }
-      console.log("Successfully joined queue.");
-      alert("Successfully joined queue.");
-   } catch (err) {
-      console.error(err);
-      alert("Something went wrong. Check console.");
-   }
-   navigate("/queue");
-  };
 
-  return (
-    <div>
-      <h1>Home page</h1>
-      <button onClick={handleQueueJoin} className="linkButton">Queue</button>
-    </div>
-  );
+      try {
+         const response = await fetch("http://localhost:5243/api/queue", {
+            method: "POST",
+            headers: {
+               "Authorization": "Bearer " + token,
+               "Content-Type": "application/json"
+            },
+         });
+
+         if (!response.ok) {
+            console.log("Status: ", response.status);
+            const errorText = await response.text();
+            console.log("Error text: ", errorText)
+            alert("Failed to join queue: " + (errorText || "Status " + response.status));
+            return;
+         }
+         console.log("Successfully joined queue.");
+         alert("Successfully joined queue.");
+      } catch (err) {
+         console.error(err);
+         alert("Something went wrong. Check console.");
+      }
+      navigate("/queue");
+   };
+
+   const handleLobbyCreation = async () => {
+      if (!lobbyID) {
+         alert("Please enter a valid lobby ID.");
+         return;
+      }
+      navigate(`/match/${lobbyID}`);
+   }
+   return (
+      <div>
+         <h1>Home page</h1>
+         <button onClick={handleQueueJoin} className="linkButton">Queue</button>
+         <div>
+            <input
+               type="number"
+               placeholder="Lobby Code"
+               value={lobbyID}
+               onChange={(e) => setlobbyID(e.target.value)}
+            />
+            <button onClick={handleLobbyCreation} className="linkButton">Lobby</button>
+         </div>
+      </div>
+   );
 }
 
 export default Home;
