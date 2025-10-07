f2aebdb8efe86c8ffb8782c3af5983473fab7ca9
Aaron Gandžumian <aaron.gandzumian@mif.stud.vu.lt>
2025-10-08 02:35:28 +0300

    Add `GameType` variable to `MatchSession` class for future game selection in lobby

--- Api/Models/MatchSession.cs
+++ Api/Models/MatchSession.cs
@@ -1,9 +1,8 @@
-namespace PSI.Api.Models
+public class MatchSession
 {
-   public class MatchSession
-   {
-      public required string Code { get; set; }
-      public List<string> Players { get; set; } = new();
-      public bool inGame { get; set; }
-   }
+   public required string Code { get; set; }
+   public required string HostConnectionId { get; set; }
+   public List<string> Players { get; set; } = new();
+   public string? GameType { get; set; }
+   public bool inGame { get; set; }
 }
