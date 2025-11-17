2d7bcf18b9ba960093b0d1c1d7f2aeb57cda3152
testyt419 <testytgaming@gmail.com>
2025-11-17 12:39:44 +0200

    100% coverage for lobby controller

--- Api.Tests/Controllers/LobbyControllerUnitTests.cs
+++ Api.Tests/Controllers/LobbyControllerUnitTests.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 
 using Api.Controllers;
+using Api.GameLogic;
 using Api.Models;
 using Api.Services;
 
@@ -31,83 +32,22 @@ public class LobbyControllerUnitTests
       };
    }
 
+   private static ControllerContext NoNameContext()
+   {
+      var user = new ClaimsPrincipal();
+
+      return new ControllerContext
+      {
+         HttpContext = new DefaultHttpContext { User = user }
+      };
+   }
+
    private static ControllerContext UnauthenticatedContext()
    {
       return new ControllerContext
       {
          HttpContext = new DefaultHttpContext { User = new ClaimsPrincipal(new ClaimsIdentity()) }
       };
-   }
-
-   [Fact]
-   public async Task CreateLobbyWithSettings_InvalidNumberOfRounds_ReturnsBadRequest()
-   {
-      var mock = new Mock<ILobbyService>();
-      var controller = new LobbyController(mock.Object)
-      {
-         ControllerContext = AuthenticatedContext(Guid.NewGuid())
-      };
-
-      var dto = new CreateLobbyDto { NumberOfRounds = 0, NumberOfPlayers = 2, RandomGames = true };
-
-      var result = await controller.CreateLobbyWithSettings(dto);
-
-      Assert.IsType<BadRequestObjectResult>(result);
-   }
-
-   [Fact]
-   public async Task CreateLobbyWithSettings_InvalidNumberOfPlayers_ReturnsBadRequest()
-   {
-      var mock = new Mock<ILobbyService>();
-      var controller = new LobbyController(mock.Object)
-      {
-         ControllerContext = AuthenticatedContext(Guid.NewGuid())
-      };
-
-      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 1, RandomGames = true };
-
-      var result = await controller.CreateLobbyWithSettings(dto);
-
-      Assert.IsType<BadRequestObjectResult>(result);
-   }
-
-   [Fact]
-   public async Task CreateLobbyWithSettings_EmptyGamesListWhenNotRandom_ReturnsBadRequest()
-   {
-      var mock = new Mock<ILobbyService>();
-      var controller = new LobbyController(mock.Object)
-      {
-         ControllerContext = AuthenticatedContext(Guid.NewGuid())
-      };
-
-      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = false, GamesList = new List<string>() };
-
-      var result = await controller.CreateLobbyWithSettings(dto);
-
-      Assert.IsType<BadRequestObjectResult>(result);
-   }
-
-   [Fact]
-   public async Task CreateLobbyWithSettings_ValidRequest_ReturnsOkWithCode()
-   {
-      var mock = new Mock<ILobbyService>();
-      mock.Setup(s => s.CreateLobbyWithSettings(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<List<string>>()))
-         .ReturnsAsync("1234");
-
-      var controller = new LobbyController(mock.Object)
-      {
-         ControllerContext = AuthenticatedContext(Guid.NewGuid())
-      };
-
-      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = true };
-
-      var result = await controller.CreateLobbyWithSettings(dto);
-
-      var ok = Assert.IsType<OkObjectResult>(result);
-      var value = ok.Value!;
-      var codeProp = value.GetType().GetProperty("Code");
-      Assert.NotNull(codeProp);
-      Assert.Equal("1234", codeProp.GetValue(value)?.ToString());
    }
 
    [Fact]
@@ -121,6 +61,22 @@ public class LobbyControllerUnitTests
 
       var result = controller.CanJoinMatch("code123");
 
+      Assert.IsType<UnauthorizedResult>(result);
+   }
+
+   [Fact]
+   public void CanJoinMatch_Unauthorized_UnauthorizedWhenContextDoesNotHaveName()
+   {
+      var userId = Guid.NewGuid();
+      var mock = new Mock<ILobbyService>();
+      mock.Setup(s => s.CanJoinLobby("code123", userId)).Returns((string?)null);
+
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = NoNameContext()
+      };
+
+      var result = controller.CanJoinMatch("code123");
       Assert.IsType<UnauthorizedResult>(result);
    }
 
@@ -181,6 +137,20 @@ public class LobbyControllerUnitTests
    }
 
    [Fact]
+   public async Task LeaveMatch_Unauthorized_NoNameReturnsUnauthorized()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = NoNameContext()
+      };
+
+      var result = await controller.LeaveMatch("code123");
+
+      Assert.IsType<UnauthorizedResult>(result);
+   }
+
+   [Fact]
    public async Task LeaveMatch_Success_ReturnsOk()
    {
       var userId = Guid.NewGuid();
@@ -216,6 +186,148 @@ public class LobbyControllerUnitTests
       var result = await controller.LeaveMatch("code123");
 
       Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_InvalidNumberOfRounds_ReturnsBadRequest()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 0, NumberOfPlayers = 2, RandomGames = true };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_InvalidNumberOfPlayers_ReturnsBadRequest()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 1, RandomGames = true };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_EmptyGamesListWhenNotRandom_ReturnsBadRequest()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = false, GamesList = new List<string>() };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_NullGamesListWhenNotRandom_ReturnsBadRequest()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = false, GamesList = null };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_EmptyOrNullGameInGamesList_ReturnsBadRequest()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = false, GamesList = new List<string> { "" } };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_NotAGameInGamesList_ReturnsBadRequest()
+   {
+      var mock = new Mock<ILobbyService>();
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = false, GamesList = new List<string> { "Game1" } };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      Assert.IsType<BadRequestObjectResult>(result);
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_ValidRequestWithRandomTrue_ReturnsOkWithCode()
+   {
+      var mock = new Mock<ILobbyService>();
+      mock.Setup(s => s.CreateLobbyWithSettings(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<List<string>>()))
+         .ReturnsAsync("1234");
+
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = true };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      var ok = Assert.IsType<OkObjectResult>(result);
+      var value = ok.Value!;
+      var codeProp = value.GetType().GetProperty("Code");
+      Assert.NotNull(codeProp);
+      Assert.Equal("1234", codeProp.GetValue(value)?.ToString());
+   }
+
+   [Fact]
+   public async Task CreateLobbyWithSettings_ValidRequestWithGamesList_ReturnsOkWithCode()
+   {
+      var mock = new Mock<ILobbyService>();
+      mock.Setup(s => s.CreateLobbyWithSettings(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<List<string>>()))
+         .ReturnsAsync("1234");
+
+      var controller = new LobbyController(mock.Object)
+      {
+         ControllerContext = AuthenticatedContext(Guid.NewGuid())
+      };
+
+      var dto = new CreateLobbyDto { NumberOfRounds = 1, NumberOfPlayers = 2, RandomGames = false, GamesList = new List<string> { "TicTacToe" } };
+
+      var result = await controller.CreateLobbyWithSettings(dto);
+
+      var ok = Assert.IsType<OkObjectResult>(result);
+      var value = ok.Value!;
+      var codeProp = value.GetType().GetProperty("Code");
+      Assert.NotNull(codeProp);
+      Assert.Equal("1234", codeProp.GetValue(value)?.ToString());
    }
 }
 
