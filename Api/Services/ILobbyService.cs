a743063ca6baf6bf2daea91dd27d79dcc888408a
testyt419 <testytgaming@gmail.com>
2025-11-26 14:55:27 +0200

    Refactor TournamentHub to utilize newly created ITournamentService and streamline lobby functionality

---
+++ Api/Services/ILobbyService.cs
@@ -0,0 +1,13 @@
+using Api.Entities;
+
+namespace Api.Services;
+
+public interface ILobbyService
+{
+   public List<User> GetPlayersInLobby(string code);
+   public Task<string?> JoinLobby(string code, User user);
+   public Task<bool> LeaveLobby(string code, Guid userId);
+   public Task<string> CreateLobbyWithSettings(int numberOfPlayers, int numberOfRounds, bool randomGames, List<string>? gamesList);
+   public string? CanJoinLobby(string code, Guid userId);
+   public bool AreAllPlayersInLobby(string code);
+}
--- Api/Services/ITournamentService.cs
+++ Api/Services/ITournamentService.cs
@@ -1,5 +1,4 @@
 using Api.Models;
-using Api.Entities;
 
 namespace Api.Services;
 
@@ -9,14 +8,8 @@ public interface ITournamentService
    public bool AddGameId(string code, Guid userId, string gameId = "");
    public bool RemoveGameId(string? code, Guid? userId);
    public bool TryGetGameId(string? code, Guid userId, out string? gameId);
-   public List<User> GetPlayersInLobby(string code);
-   public Task<string?> JoinLobby(string code, User user);
-   public Task<bool> LeaveLobby(string code, Guid userId);
-   public Task<string> CreateLobbyWithSettings(int numberOfPlayers, int numberOfRounds, bool randomGames, List<string>? gamesList);
-   public string? CanJoinLobby(string code, Guid userId);
    public TournamentSession? GetTournamentSession(string code);
    public void MarkGameAsEnded(string code, string gameId);
    public bool AreAllGamesEnded(string code);
    public void ResetRoundEndTracking(string code);
-   public bool AreAllPlayersInLobby(string code);
 }
