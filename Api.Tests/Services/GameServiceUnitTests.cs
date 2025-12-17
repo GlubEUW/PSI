bfa0f79bb11c8371b9ab7e48654c48e1d8eba55e
GlubEUW <mindaugas.kalvinskas@gmail.com>
2025-12-17 15:31:17 +0200

    Some more tests

--- Api.Tests/Services/GameServiceUnitTests.cs
+++ Api.Tests/Services/GameServiceUnitTests.cs
@@ -1,6 +1,9 @@
 using Api.Entities;
 using Api.Enums;
 using Api.Services;
+using Api.GameLogic;
+
+using Moq;
 
 namespace Api.Tests.Services;
 
@@ -50,5 +53,31 @@ public class GameServiceUnitTests
       var game = svc.StartGame(GameType.TicTacToe, players);
       Assert.NotNull(game);
       Assert.Equal(GameType.TicTacToe, game!.GameType);
+   }
+
+   [Fact]
+   public void IsValidGameType_IsCaseInsensitive_ReturnsTrue()
+   {
+      var svc = (GameService)TestHelpers.CreateGameService();
+      Assert.True(svc.IsValidGameType("connectfour"));
+      Assert.True(svc.IsValidGameType("rockpaperscissors"));
+      Assert.True(svc.IsValidGameType("tictactoe"));
+   }
+
+   [Fact]
+   public void StartGame_ReturnsNull_WhenFactoryThrows()
+   {
+      var factory = new Mock<IGameFactory>();
+      factory.SetupGet(f => f.ValidGameTypes).Returns((IReadOnlySet<GameType>)new HashSet<GameType> { GameType.TicTacToe });
+      factory.Setup(f => f.CreateGame(It.IsAny<GameType>(), It.IsAny<List<User>>())).Throws(new InvalidOperationException("boom"));
+
+      var svc = new GameService(factory.Object);
+      var players = new List<User>
+      {
+         new Guest { Id = Guid.NewGuid(), Name = "a" },
+         new Guest { Id = Guid.NewGuid(), Name = "b" }
+      };
+
+      Assert.Null(svc.StartGame(GameType.TicTacToe, players));
    }
 }
